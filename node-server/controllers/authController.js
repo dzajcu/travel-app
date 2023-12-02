53868e3f78d50040d673a56175503c6414bc102c
dzajcu <dawidz1231@gmail.com>
2023-12-02 20:02:53 +0100

    changed expires to maxAge in cookie options

--- node-server/controllers/authController.js
+++ node-server/controllers/authController.js
@@ -14,12 +14,13 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
     const cookieOptions = {
-        expires: new Date(
-            Date.now() +
-                process.env.JWT_COOKIE_EXPIRES_IN.trim() * 24 * 60 * 60 * 1000
-        ),
+        // expires: new Date(
+        //     Date.now() +
+        //         process.env.JWT_COOKIE_EXPIRES_IN.trim() * 24 * 60 * 60 * 1000
+        // ),
         httpOnly: true,
         secure: process.env.NODE_ENV.trim() === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 30, // would expire after 1 month
     };
 
     res.cookie("jwt", token, cookieOptions);
